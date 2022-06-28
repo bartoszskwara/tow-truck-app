d4451763c5c43053465adf381dc8dbf79962ece4
Bartosz Skwara <bartoszskwara95@gmail.com>
2022-06-28 20:54:26 +0200

    Add custom scrollbar

--- src/pages/Stations/StationsList/StationCard/components/SettingsPanel/History/components/HistoryList.tsx
+++ src/pages/Stations/StationsList/StationCard/components/SettingsPanel/History/components/HistoryList.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import SimpleBar from 'simplebar-react';
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+} from '@mui/material';
 import { ReactComponent as AccidentIcon } from 'assets/icons/accident.svg';
 import Text from 'components/Text';
 import { History, Sx } from 'types';
 import AddressBox from './AddressBox';
+import 'simplebar/dist/simplebar.min.css';
 
 interface Props {
     history: History[];
@@ -13,56 +21,56 @@ interface Props {
 }
 
 const HistoryList = ({ history, selected, onSelect, sx }: Props) => (
-    <List
-        sx={[
-            {
-                marginY: (theme) => theme.spacing(1),
-                background: (theme) => theme.palette.background.secondary,
-                maxHeight: (theme) => theme.spacing(30),
-                overflow: 'auto',
-            },
-            ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-    >
-        {history.map((i) => (
-            <ListItem
-                key={`history_${i.id}`}
-                onClick={() => onSelect(i)}
+    <Box sx={[...(Array.isArray(sx) ? sx : [sx])]}>
+        <SimpleBar style={{ maxHeight: '300px' }}>
+            <List
                 sx={{
-                    cursor: 'pointer',
-                    ...(selected?.id === i.id
-                        ? {
-                              background: (theme) =>
-                                  theme.palette.background.primary,
-                          }
-                        : {}),
+                    marginY: (theme) => theme.spacing(1),
+                    background: (theme) => theme.palette.background.secondary,
                 }}
             >
-                <ListItemAvatar>
-                    <AccidentIcon />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={
-                        <Text
-                            text={<AddressBox {...i.address} />}
-                            variant="bold"
+                {history.map((i) => (
+                    <ListItem
+                        key={`history_${i.id}`}
+                        onClick={() => onSelect(i)}
+                        sx={{
+                            cursor: 'pointer',
+                            ...(selected?.id === i.id
+                                ? {
+                                      background: (theme) =>
+                                          theme.palette.background.primary,
+                                  }
+                                : {}),
+                        }}
+                    >
+                        <ListItemAvatar>
+                            <AccidentIcon />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <Text
+                                    text={<AddressBox {...i.address} />}
+                                    variant="bold"
+                                />
+                            }
+                            secondary={
+                                <Text
+                                    text={new Intl.DateTimeFormat().format(
+                                        new Date(i.datetime)
+                                    )}
+                                    sx={{
+                                        color: (theme) =>
+                                            theme.palette.text.secondary,
+                                    }}
+                                    variant="bold"
+                                />
+                            }
                         />
-                    }
-                    secondary={
-                        <Text
-                            text={new Intl.DateTimeFormat().format(
-                                new Date(i.datetime)
-                            )}
-                            sx={{
-                                color: (theme) => theme.palette.text.secondary,
-                            }}
-                            variant="bold"
-                        />
-                    }
-                />
-            </ListItem>
-        ))}
-    </List>
+                    </ListItem>
+                ))}
+            </List>
+        </SimpleBar>
+    </Box>
 );
 
 export default HistoryList;
